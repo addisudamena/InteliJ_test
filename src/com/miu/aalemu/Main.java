7e5a7ebe88b99f9c2a2cc04779d43f794d770cb8
Addisu <addisudamena49@gmail.com>
2021-03-22 17:16:56 -0500

    Commit message

--- src/com/miu/aalemu/Main.java
+++ src/com/miu/aalemu/Main.java
@@ -1,6 +1,10 @@
 package com.miu.aalemu;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String []a){
         System.out.println("Hellow world");
+        System.out.println("a = " + Arrays.deepToString(a));
     }
 }
